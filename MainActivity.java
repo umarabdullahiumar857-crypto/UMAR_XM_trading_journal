a3902e70b7226f4bcad7477d9eac6d614e362db3
umarabdullahiumar857-crypto <umarabdullahiumar857@gmail.com>
2026-08-20 11:14:52 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.umarxm.tradingjournal;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
