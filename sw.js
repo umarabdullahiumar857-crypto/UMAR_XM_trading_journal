6c9f0f4fe25c3ff20399eb262671186303602159
umarabdullahiumar857-crypto <umarabdullahiumar857@gmail.com>
2026-08-20 11:42:58 +0100

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "umar-xm-v2-cache";
+
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
